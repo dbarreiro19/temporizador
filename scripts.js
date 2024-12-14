fa33f45d429b4802e600d19c34c12714008d34eb
Diego Barreiro <142553059+dbarreiro19@users.noreply.github.com>
2024-12-14 15:47:55 +0000

    Correções na contagem tempo, inclusão de rodapé e cor do título alterada.

--- scripts.js
+++ scripts.js
@@ -1,33 +1,41 @@
 let res = document.querySelector(".res")
 let title_app = document.querySelector("#title-app")
 
+function formatar_tempo(tempo) {
+
+    if (tempo >= 0 && tempo <= 9) {
+        var tempo_formatado = tempo.toString()
+        tempo_formatado = "0" + tempo_formatado
+    } else {
+        var tempo_formatado  = tempo.toString()
+    }    
+    return tempo_formatado
+
+}
+
 function count() {
 
     let data_atual = new Date()
     data_atual = data_atual.getTime()
-
-    let data_final = new Date("2025-04-17")
+    
+    let data_final = new Date("2025-04-16 18:48:00")
     data_final = data_final.getTime()
     
-    let diferenca_ms = data_final - data_atual
+    let ms_restante = new Date(data_final - data_atual)
 
-    let dias = Math.round(diferenca_ms / 86400000)
+    let dias = Math.round(ms_restante / 84600000)
+    let horas = ms_restante.getUTCHours()
+    let minutos = ms_restante.getUTCMinutes()
+    let segundos = ms_restante.getUTCSeconds()
 
-    let horas = Math.round(diferenca_ms / 3600000)
+    res.innerHTML = `${dias} dias ${formatar_tempo(horas)}h ${formatar_tempo(minutos)}m ${formatar_tempo(segundos)}s`
 
-    let minutos = Math.round(diferenca_ms / 60000)
-    
-    let segundos = Math.round(diferenca_ms / 1000)
-
-    res.innerHTML = `${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
-
-    if (diferenca_ms <= 0) {        
-        title_app.innerHTML = "FIM!"
+    if (ms_restante <= 0) {        
+        title_app.innerHTML = "GAMEOVER!"
         window.clearInterval(count_app)
     }
 
 }
 
 let count_app = window.setInterval(count, 1000)
-
 count()
